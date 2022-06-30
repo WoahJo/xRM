00e04e501299861cbd78c7dc2613476cfd3cbed5
WoahJo <ccnettacc@gmail.com>
2022-06-30 12:26:34 -0400

    added decimal to total, detects invalid input

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function appendTable (weight, reps) {
     const addWeight = document.createElement('td'); 
     const addReps = document.createElement('td'); 
 
-    addWeight.textContent = weight +"lbs"; 
+    addWeight.textContent = weight; 
     addReps.textContent = reps + "RM";
 
     row.append(addReps, addWeight); 
@@ -22,13 +22,17 @@ function calcMax () {
         const weight = document.getElementsByClassName('weight')[0].value;
         const reps = document.getElementsByClassName('reps')[0].value;
 
-        let max = Math.floor(weight * (36/(37 - reps) ));
-        appendTable(max, 1);
+        let max = Math.round(weight * (36/(37 - reps))* 10) / 10;
+        if(isNaN(max))(alert('Numbers only.'));
+        else{
+            appendTable(max, 1);
 
-        for(let i = 2; i < 11; i++){
-            let maxList = Math.floor((max * (37 - i)) / 36);
-            appendTable(maxList, i);
+            for(let i = 2; i < 11; i++){
+                let maxList = Math.round((max * (37 - i)) / 36 * 10) / 10;
+                appendTable(maxList, i);
+            }
         }
+
 };
 
 (function calculate(){
